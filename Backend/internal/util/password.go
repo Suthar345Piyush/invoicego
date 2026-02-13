3c98eb5c36431af779eb3cf2876b0d259fdfa4a8
Suthar345Piyush <piyushsuthar524@gmail.com>
2026-02-13 13:41:12 +0530

    fix : Function typo causing error

--- Backend/internal/util/password.go
+++ Backend/internal/util/password.go
@@ -8,14 +8,18 @@ import (
 
 // password hashing function
 
-func HashPassword(password string) (string error) {
+func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
 	return string(bytes), err
+
 }
 
 // checking the password
 
-func checkPassword(password, hash string) bool {
+// lowercase function - private function
+// Uppercase lettered function - exported function - which can be exported and used at any other place
+
+func CheckPassword(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
