4ba69e5bafcade3367e9e3b71302f3a1e7b7eadd
Suthar345Piyush <piyushsuthar524@gmail.com>
2026-02-18 11:20:50 +0530

    wrote client handler

--- Backend/internal/handler/client_handler.go
+++ Backend/internal/handler/client_handler.go
@@ -1,6 +1,18 @@
 package handler
 
-import "github.com/Suthar345Piyush/invoicego/internal/service"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/Suthar345Piyush/invoicego/internal/domain"
+	"github.com/Suthar345Piyush/invoicego/internal/middleware"
+	"github.com/Suthar345Piyush/invoicego/internal/service"
+	"github.com/Suthar345Piyush/invoicego/internal/util"
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+)
 
 // handler
 
@@ -15,3 +27,180 @@ func NewClientHandler(clientService *service.ClientService) *ClientHandler {
 }
 
 // creating client function
+
+func (h *ClientHandler) CreateClient(w http.ResponseWriter, r *http.Request) {
+
+	claims, ok := middleware.GetUserFromContext(r.Context())
+
+	if !ok {
+		util.WriteError(w, http.StatusUnauthorized, errors.New("unauthorized"))
+		return
+	}
+
+	var req domain.CreateClientRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		util.WriteError(w, http.StatusBadRequest, domain.ErrInvalidInput)
+		return
+	}
+
+	// validating the input
+
+	if err := util.ValidateStruct(&req); err != nil {
+		util.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	client, err := h.clientService.CreateClient(claims.UserID, &req)
+
+	if err != nil {
+		util.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	util.WriteSuccess(w, http.StatusCreated, client, "Client created successfully")
+}
+
+// function to get the client
+
+func (h *ClientHandler) GetClient(w http.ResponseWriter, r *http.Request) {
+
+	claims, ok := middleware.GetUserFromContext(r.Context())
+
+	if !ok {
+		util.WriteError(w, http.StatusUnauthorized, errors.New("unauthorized"))
+		return
+	}
+
+	clientIDStr := chi.URLParam(r, "id")
+
+	//  parsing the client id string
+
+	clientID, err := uuid.Parse(clientIDStr)
+
+	if err != nil {
+		util.WriteError(w, http.StatusBadRequest, errors.New("invalid client ID"))
+	}
+
+	client, err := h.clientService.GetClientByID(claims.UserID, clientID)
+
+	if err != nil {
+		util.WriteError(w, http.StatusNotFound, err)
+	}
+
+	util.WriteSuccess(w, http.StatusOK, client, "Client retrieved successfully")
+}
+
+// listing all the clients
+
+func (h *ClientHandler) ListClients(w http.ResponseWriter, r *http.Request) {
+
+	claims, ok := middleware.GetUserFromContext(r.Context())
+
+	if !ok {
+		util.WriteError(w, http.StatusUnauthorized, errors.New("unauthorized"))
+		return
+	}
+
+	// parsing  pagination parameters
+
+	page, _ := strconv.Atoi(r.URL.Query().Get("page"))
+
+	pageSize, _ := strconv.Atoi(r.URL.Query().Get("page_size"))
+
+	if page < 1 {
+		page = 1
+	}
+
+	if pageSize < 1 {
+		pageSize = 20
+	}
+
+	clients, err := h.clientService.GetClientsByUserID(claims.UserID, page, pageSize)
+
+	if err != nil {
+		util.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	util.WriteSuccess(w, http.StatusOK, clients, "Clients retrieved successfully")
+
+}
+
+// updating the client
+
+func (h *ClientHandler) UpdateClient(w http.ResponseWriter, r *http.Request) {
+
+	claims, ok := middleware.GetUserFromContext(r.Context())
+
+	if !ok {
+		util.WriteError(w, http.StatusUnauthorized, errors.New("unauthorized"))
+	}
+
+	clientIDStr := chi.URLParam(r, "id")
+
+	clientID, err := uuid.Parse(clientIDStr)
+
+	if err != nil {
+		util.WriteError(w, http.StatusBadRequest, errors.New("invalid client ID"))
+		return
+	}
+
+	// requesting to update the client
+
+	var req domain.UpdateClientRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		util.WriteError(w, http.StatusBadRequest, domain.ErrInvalidInput)
+		return
+	}
+
+	// validating input
+
+	if err := util.ValidateStruct(&req); err != nil {
+		util.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// to update we need userID , clientID and req address
+
+	client, err := h.clientService.UpdateClient(claims.UserID, clientID, &req)
+
+	if err != nil {
+		util.WriteError(w, http.StatusInternalServerError, err)
+	}
+
+	util.WriteSuccess(w, http.StatusOK, client, "Client updated successfully")
+}
+
+// deleting the client
+
+func (h *ClientHandler) DeleteClient(w http.ResponseWriter, r *http.Request) {
+
+	claims, ok := middleware.GetUserFromContext(r.Context())
+
+	if !ok {
+		util.WriteError(w, http.StatusUnauthorized, errors.New("unauthorized"))
+		return
+	}
+
+	clientIDStr := chi.URLParam(r, "id")
+
+	clientID, err := uuid.Parse(clientIDStr)
+
+	if err != nil {
+		util.WriteError(w, http.StatusBadRequest, errors.New("invalid client ID"))
+		return
+	}
+
+	//deleting the client
+
+	err = h.clientService.DeleteClient(claims.UserID, clientID)
+
+	if err != nil {
+		util.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	util.WriteSuccess(w, http.StatusOK, nil, "Client deleted successfully")
+
+}
